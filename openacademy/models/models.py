da9349d08cdfd6eba8ebae735a7fa7f60a79c597
Evelyn Mora <mora.eve.c@gmail.com>
2019-05-30 03:02:15 +0000

    [REF] openacademy: Video de restricciones y funcionamiento

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
+#from psycopg2 import IntegrityError
 import time
 
 def get_uid(self, *a):
@@ -18,8 +19,35 @@ class Course(models.Model):
 		default=get_uid)
 	session_ids = fields.One2many('openacademy.session', 'course_id')
 	
+	_sql_constraints = [
+		('name_description_check',
+		'CHECK(name != description)',
+		"The title of the course should not be the description",
+		),
+		('name_unique',
+		 'UNIQUE(name)',
+		 "The course title must be unique",
+		),
+	]
+	
+	def copy(self, default=None):
+		if default is None:
+			default = {}
+		copied_count = self.search_count([
+			('name','ilike','Copy of %s%%' % (self.name))])
+		if not copied_count:
+			new_name = "Copy of %s" % (self.name)
+		else:
+			new_name = "Copy of %s (%s)" % (self.name, copied_count)
+		default['name'] = new_name
+#		try:
+		return super(Course, self).copy(default)
+#		except IntegrityError:
+			
+	
 class Session(models.Model):
 	_name = 'openacademy.session'
+	_description = '''  '''
 	name = fields.Char(required=True)
 	start_date = fields.Date(default=fields.Date.today)
 	datetime_test = fields.Datetime(default=fields.Datetime.now)
@@ -57,3 +85,10 @@ class Session(models.Model):
 					}
 				}
 		self.active = True
+		
+	@api.constrains('instructor_id', 'attendee_ids')
+	def _check_instructor_not_in_attendees(self):
+		for record in self.filtered('instructor_id'):
+			if record.instructor_id in record.attendee_ids:
+				raise exceptions.ValidationError(
+					"A session's instructor can't be an attendee")
